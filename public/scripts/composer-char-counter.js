b358a12da49ba149149b4d81c3e61710a504dd86
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-16 23:59:55 -0500

    created the tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+// console.log ("Do I work?")
+$(document).ready(function() {
+  $('#tweet-text').on('keyup',function(){  
+   let characterCount = 140 - ($(this).val().length)
+    $(this).parents("form").children("output").text(characterCount)
+    if(characterCount < 0){
+      $(this).parents("form").children("output").addClass('red-counter')
+    }
+    else{
+      $(this).parents("form").children("output").removeClass('red-counter')
+    }
+})
+});
+
+// [ ] A container element that contains the tweets
+// [ ] The styling of the tweet, as shown in the first screenshot below
+// [ ] The hover state of the tweet where the stylistic changes occur (as mentioned in the second screenshot below)
+// [ ] The time that has passed since the tweet was created (e.g. 10 days ago)
