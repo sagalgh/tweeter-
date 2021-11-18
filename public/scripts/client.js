3fd611feb3394265f043dee24f13442cd55faec7
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-17 23:19:40 -0500

    added refetch tweets upon submission feature

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,45 +5,31 @@
 // //  */
 // const $tweet = $(`<article class="tweet">Hello world</article>`);
 
+// const { text } = require("express");
+
 // const { text } = require("body-parser")
 
 // Test / driver code (temporary). Eventually will get this from the server.
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 const renderTweets = function(tweets) {
+  $('.tweets-container').empty();
   for (let tweet of tweets){
     // calls createTweetElement for each tweet
     const $tweet = createTweetElement(tweet)
     // takes return value and appends it to the tweets container
-    $('.tweets-container').append($tweet)
+    $('.tweets-container').prepend($tweet)
   }
 }
 const createTweetElement = function(tweetData){
- const $tweet =  $("<article>").addClass("tweet");
-    $tweet.append(`
+//  const $tweet =  $("<article>").addClass("tweet");
+
+    // $tweet.append(`
+    const tweet= `
   <div>
       <header class = "tweet-header">
         <span><img src="${tweetData.user.avatars}"></img> ${tweetData.user.name}</span>
@@ -62,28 +48,50 @@ const createTweetElement = function(tweetData){
         </span>
       </footer>
   </div>
-  `)
-  return $tweet
+  `
+  return tweet
 }
 // const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
 $(document).ready(function() {
+  function loadTweets(){
+    $.ajax({
+      type: "GET", 
+      url:"/tweets", 
+      dataType:"json"
+    }).then((res)=>{
+      renderTweets(res);
+      console.log("response--",res)
+      })
+    };  
+  loadTweets();
   $('#tweet-submit').on('submit', function(evt){
+
     evt.preventDefault();
     const val= $(this).serialize();
+    
+    if($("#tweet-text").val().length === 0){
+      alert("Error- Field empty")
+    }
+    if($("#tweet-text").val().length > 140){
+      alert("Error- number of character exceeded")
+    }
+//  console.log(evt.target.tweet.value)
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: val
     }).then((res)=>{
-        console.log("VAL-----",val)
+        // console.log("VAL-----",val)
+        loadTweets()
       })
-  //  $.post("/tweets", val).then(()=>{
-  //   console.log("VAL-----",val)
-  // })
+    
 })
-renderTweets(data)
+
+
+
+
 });
  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
